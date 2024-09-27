167f238ae7d64b2bb2fa4753d5c590516f4d4acc
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-27 17:11:28 -0400

    feat: add basic game functionality
    At this point, the cards properly update state when clicked for the
    first time, current and best score are properly tracked and updated, and
    the GameDisplay is rebuilt from scratch on a loss, resetting the state
    of all Card components. Next steps are retreiving the images for display
    and creating a function to randomize placements on each Card click.

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ function App() {
 
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [round, setRound] = useState(1);
 
   return (
     <>
       <header id='header'>Memory Game
         <ScoreDisplay currentScore={currentScore} bestScore={bestScore} />
       </header>
-      <GameDisplay />
+      <GameDisplay key={round} round={round} currentScore={currentScore} bestScore={bestScore} setRound={setRound} setCurrentScore={setCurrentScore} setBestScore={setBestScore} />
     </>
   )
 }
--- src/components/GameDisplay.jsx
+++ src/components/GameDisplay.jsx
@@ -1,27 +1,46 @@
+import { useState } from 'react'
 import '../styles/utility.css'
 import '../styles/GameDisplay.css'
 
-export default function GameDisplay() {
+export default function GameDisplay({round, currentScore, bestScore, setCurrentScore, setBestScore, setRound}) {
+
     return (
         <div id="game-display">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
+            <Card round={round} currentScore={currentScore} bestScore={bestScore} setCurrentScore={setCurrentScore} setBestScore={setBestScore} setRound={setRound} />
         </div>
     )
 }
 
-function Card() {
+function Card({round, currentScore, bestScore, setCurrentScore, setBestScore, setRound }) {
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        if (clicked) {
+            setCurrentScore(0);
+            if (currentScore > bestScore) {
+                setBestScore(currentScore);
+            }
+            setRound(round + 1)
+        } else {
+            setClicked(!clicked);
+            setCurrentScore(currentScore + 1)
+        }
+    }
+
     return (
-        <div className="card"></div>
+        <div className="card"
+            onClick={handleClick}>
+        </div>
     )
 }
