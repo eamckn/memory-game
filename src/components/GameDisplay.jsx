9be48ef1b167c84650481f205340329683263960
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-09-28 15:57:47 -0400

    style: remove commented out code

--- src/components/GameDisplay.jsx
+++ src/components/GameDisplay.jsx
@@ -2,20 +2,6 @@ import { useState, useEffect } from 'react'
 import '../styles/utility.css'
 import '../styles/GameDisplay.css'
 
-// const cardsInfo = [{id: "Ruins-GS", name:"Ruins Greatsword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/ruins_greatsword_colossal_swords_elden_ring_wiki_guide_200px.png"},
-//     {id: "Eclipse-Shotel", name:"Eclipse Shotel", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/eclipse_shotel_curved_sword_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "Grafted-Blade-GS", name:"Grafted Blade Greatsword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/grafted_blade_greatsword_colossal_swords_elden_ring_wiki_guide_200px.png"},
-//     {id: "SONAF", name:"Sword of Night and Flame", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/sword_of_night_and_flame_straight_sword_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "Marias-Executioner-GS", name:"Marais Executioner's Sword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/marais_executioners_sword_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "DMGS", name:"Dark Moon Greatsword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/dark_moon_greatsword_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "Devourer's-Scepter", name:"Devourer's Scepter", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/devourers_scepter_warhammer_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "GOGS", name:"Golden Order Greatsword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/golden_order_greatsword_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "Bolt-of-Gransax", name:"Bolt of Gransax", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/bolt_of_gransax_spear_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "Black-Knife", name:"Black Knife", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/black_knife_dagger_weapon_elden_ring_wiki_guide_200px.png"},
-//     {id: "Godslayer-GS", name:"Godslayer's Greatsword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/godslayers_greatsword_colossal_swords_elden_ring_wiki_guide_200px.png"},
-//     {id: "AMOGS", name:"Ancient Meteoric Ore Greatsword", url: "https://eldenring.wiki.fextralife.com/file/Elden-Ring/ancient_meteoric_ore_greatsword_elden_ring_shadow_of_the_erdtree_dlc_wiki_guide_200px.png"},
-// ]
-
 
 export default function GameDisplay({ round,
     currentScore,
@@ -24,7 +10,6 @@ export default function GameDisplay({ round,
     setBestScore,
     setRound }) {
     
-    //const [init, setInit] = useState(false);
     const [cardInfo, setCardInfo] = useState([])
 
     const randomizeCards = (array = cardInfo) => {
@@ -51,26 +36,6 @@ export default function GameDisplay({ round,
             member.name === "White Zetsu")
     }
 
-    // async function getData() {
-    //     const url = 'https://narutodb.xyz/api/akatsuki?limit=44';
-    //     try {
-    //         let response = await fetch(url);
-    //         let json = await response.json();
-    //         //console.log(typeof json)
-    //         //console.log(json);
-    //         //console.log(json.akatsuki)
-    //         let akatuski = json.akatsuki.map((member) => (
-    //             {id: member.id, name: member.name, url: member.images[0]}
-    //         ))
-    //         akatuski = filterAkastuki(akatuski)
-    //         console.log(akatuski)
-    //         setCardInfo(akatuski);
-    //     } 
-    //     catch (error){
-    //         console.error(error.message)
-    //     }
-    // }
-
     if (cardInfo.length !== 0) {
         randomizeCards();
     }
@@ -81,9 +46,6 @@ export default function GameDisplay({ round,
         try {
             let response = await fetch(url);
             let json = await response.json();
-            //console.log(typeof json)
-            //console.log(json);
-            //console.log(json.akatsuki)
             let akatuski = json.akatsuki.map((member) => (
                 {id: member.id, name: member.name, url: member.images[0]}
             ))
